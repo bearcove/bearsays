17f42d2c8148712f9fa22ee0657de7680328b117
Amos Wenger <amos@bearcove.net>
2025-03-22 15:32:45 +0100

    Update bearsays formula to v2.3.3

--- Formula/bearsays.rb
+++ Formula/bearsays.rb
@@ -4,15 +4,15 @@
 class Bearsays < Formula
   desc "Cool bear says stuff"
   homepage "https://code.bearcove.cloud/bearcove/bearsays"
-  version "2.3.2"
+  version "2.3.3"
   license "MIT+Apache-2.0"
 
   if OS.mac?
-    url "https://code.bearcove.cloud/api/packages/bearcove/generic/bearsays/v2.3.2/aarch64-apple-darwin.tar.xz", headers: ["Authorization: token 84b4b6c143f3c96dc56dbb3b098646dea1b57485"]
-    sha256 "0574dd13edc137d5fe9aa550b86795b930bceb0bf8c74017253c150c2833e92c"
+    url "https://code.bearcove.cloud/api/packages/bearcove/generic/bearsays/v2.3.3/aarch64-apple-darwin.tar.xz", headers: ["Authorization: token 84b4b6c143f3c96dc56dbb3b098646dea1b57485"]
+    sha256 "e2a20e75ca83c301349ab09dcc7cb7ec5a694d055c9d00db9066ea9ec3ff248b"
   elsif OS.linux?
-    url "https://code.bearcove.cloud/api/packages/bearcove/generic/bearsays/v2.3.2/x86_64-unknown-linux-gnu.tar.xz", headers: ["Authorization: token 84b4b6c143f3c96dc56dbb3b098646dea1b57485"]
-    sha256 "66f6334637a32f03af95162d8a23887e105929b9b9097efdbe41600fdd0d64c7"
+    url "https://code.bearcove.cloud/api/packages/bearcove/generic/bearsays/v2.3.3/x86_64-unknown-linux-gnu.tar.xz", headers: ["Authorization: token 84b4b6c143f3c96dc56dbb3b098646dea1b57485"]
+    sha256 "9c52620c95665e91427376bc0f0560464631ab08a7fd9a75487aa1894464946a"
   end
 
   def install
